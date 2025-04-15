55bf2a9fdd77764582dc30baaed904e174ebeb57
Nallie Than <zhengyi.than@gmail.com>
2025-04-15 21:48:28 +0800

    Weather react.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <div className='flex items-center justify-center min-h-screen'>
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Weather.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { IoSearch } from "react-icons/io5";
+import sun from '../assets/sun.png'
+import wind from '../assets/wind.png'
+import humidity from '../assets/humidity.png'
+
+const Weather = () => {
+  return (
+    <div className='flex flex-col gap-8 bg-gradient-to-b from-blue-800 to-white shadow-2xl h-[400px] w-[50%] p-10'>
+
+      <div className='flex items-center justify-center bg-white w-[60%] mx-auto rounded-full'>
+        <input type="text" placeholder='Search' className='w-full pl-4 h-8 text-[14px] outline-none bg-transparent'/>
+
+        <IoSearch className='h-8 w-8 p-[6px] bg-[#004aad] text-white rounded-full cursor-pointer'/>
+      </div>
+
+      <div className='flex items-center flex-col'>
+        <img src={sun} alt="" className='w-[90px] mb-2'/>
+        <p className='text-3xl'>New York</p>
+        <p className='text-3xl'>23°C</p>
+      </div>
+
+      <div className='flex gap-8 justify-between'>
+        <div className='flex items-center gap-2'>
+          <img src={wind} alt="" className='w-[40px]'/>
+          <div>
+            <p className='text-[12px] font-semibold'>Wind Speed</p>
+            <p>3.5 Km/h</p>
+          </div>
+        </div>
+
+        <div className='flex items-center'>
+          <div>
+            <p className='text-[12px] font-semibold'>Feels Like</p>
+            <p>20°C</p>
+          </div>
+        </div>
+
+        <div className='flex items-center gap-2'>
+          <img src={humidity} alt="" className='w-[40px]'/>
+          <div>
+            <p className='text-[12px] font-semibold'>Humidity</p>
+            <p>90%</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    
+  )
+}
+
+export default Weather
